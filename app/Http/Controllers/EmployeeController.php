711b12b0360225a614908adda0bdf3d40eddd130
Satrio Tegar <tegarn78@gmail.com>
2023-07-03 16:28:28 +0700

    Modul-9 Framework

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 use App\Models\Employee;
 use App\Models\Position;
-use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class EmployeeController extends Controller
 {
@@ -27,25 +29,14 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        // $pageTitle = 'Employee List';
-        // // RAW SQL QUERY
-        //     $employees = DB::select('
-        //         select *, employees.id as employee_id, positions.name as
-        //         position_name
-        //         from employees
-        //         left join positions on employees.position_id = positions.id
-        //         ');
-        //         return view('employee.index', [
-        //         'pageTitle' => $pageTitle,
-        //         'employees' => $employees
-        //     ]);
+
         $pageTitle = 'Employee List';
-// ELOQUENT
-$employees = Employee::all();
-return view('employee.index', [
-    'pageTitle' => $pageTitle,
-    'employees' => $employees
-]);
+    // ELOQUENT
+    $employees = Employee::all();
+    return view('employee.index', [
+        'pageTitle' => $pageTitle,
+        'employees' => $employees
+    ]);
 
         }
 
@@ -55,65 +46,70 @@ return view('employee.index', [
      */
     public function create()
     {
-        // $pageTitle = 'Create Employee';
-        // // RAW SQL Query
-        // $positions = DB::select('select * from positions');
-        // return view('employee.create', compact('pageTitle', 'positions'));
-
         $pageTitle = 'Create Employee';
         // ELOQUENT
         $positions = Position::all();
         return view('employee.create', compact('pageTitle', 'positions'));
     }
 
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
+{
+    $messages = [
+        'required' => ':Attribute harus diisi.',
+        'email' => 'Isi :attribute dengan format yang benar',
+        'numeric' => 'Isi :attribute dengan angka'
+    ];
 
-        $messages = [
-            'required' => ':Attribute harus diisi.',
-            'email' => 'Isi :attribute dengan format yang benar',
-            'numeric' => 'Isi :attribute dengan angka'
-            ];
-            $validator = Validator::make($request->all(), [
-            'firstName' => 'required',
-            'lastName' => 'required',
-            'email' => 'required|email',
-            'age' => 'required|numeric',
-            ], $messages);
-            if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-            }
-            // ELOQUENT
-            $employee = New Employee;
-            $employee->firstname = $request->firstName;
-            $employee->lastname = $request->lastName;
-            $employee->email = $request->email;
-            $employee->age = $request->age;
-            $employee->position_id = $request->position;
-            $employee->save();
-            return redirect()->route('employees.index');
+    $validator = Validator::make($request->all(), [
+        'firstName' => 'required',
+        'lastName' => 'required',
+        'email' => 'required|email',
+        'age' => 'required|numeric',
+    ], $messages);
+
+    if ($validator->fails()) {
+        return redirect()->back()->withErrors($validator)->withInput();
     }
+
+    // Get File
+    $file = $request->file('cv');
+
+    if ($file != null) {
+        $originalFilename = $file->getClientOriginalName();
+        $encryptedFilename = $file->hashName();
+
+        // Store File
+        $file->store('public/files');
+    }
+
+    // ELOQUENT
+    $employee = New Employee;
+    $employee->firstname = $request->firstName;
+    $employee->lastname = $request->lastName;
+    $employee->email = $request->email;
+    $employee->age = $request->age;
+    $employee->position_id = $request->position;
+
+    if ($file != null) {
+        $employee->original_filename = $originalFilename;
+        $employee->encrypted_filename = $encryptedFilename;
+    }
+
+    $employee->save();
+
+    return redirect()->route('employees.index');
+}
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        // $pageTitle = 'Employee Detail';
-        // // RAW SQL QUERY
-        // $employee = collect(DB::select('
-        //     select *, employees.id as employee_id, positions.name as
-        //     position_name
-        //     from employees
-        //     left join positions on employees.position_id = positions.id
-        //     where employees.id = ?
-        // ', [$id]))->first();
-        // return view('employee.show', compact('pageTitle', 'employee'));
         $pageTitle = 'Employee Detail';
+
     // ELOQUENT
     $employee = Employee::find($id);
     return view('employee.show', compact('pageTitle', 'employee'));
@@ -125,61 +121,104 @@ return view('employee.index', [
     public function edit(string $id)
     {
         $pageTitle = 'Edit Employee';
+
     // ELOQUENT
     $positions = Position::all();
     $employee = Employee::find($id);
+
     return view('employee.edit', compact('pageTitle', 'positions',
     'employee'));
     }
 
-
-
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         $messages = [
-            'required' => ':Attribute harus diisi.',
+            'required' => 'Attribute harus diisi',
             'email' => 'Isi :attribute dengan format yang benar',
             'numeric' => 'Isi :attribute dengan angka'
-            ];
-            $validator = Validator::make($request->all(), [
+        ];
+        $validator = Validator::make($request->all(), [
             'firstName' => 'required',
             'lastName' => 'required',
             'email' => 'required|email',
-            'age' => 'required|numeric',
-            ], $messages);
-            if ($validator->fails()) {
+            'age' => 'required|numeric'
+        ], $messages);
+
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Get File
+    $file = $request->file('cv');
+
+    if ($file != null) {
+        $originalFilename = $file->getClientOriginalName();
+        $encryptedFilename = $file->hashName();
+
+        // Store File
+        $file->store('public/files');
+    }
+
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+
+        if ($request->hasFile('cv')) {
+            $file = $request->file('cv');
+
+            //simpan file baru
+            $file->store('public/files');
+
+            //Delete
+            Storage::delete('public/files/'.$employee->encrypted_filename);
+
+            //Update nama file baru dalam model
+            if ($file != null) {
+
+            $employee->original_filename = $originalFilename;
+            $employee->encrypted_filename = $encryptedFilename;
             }
-            // ELOQUENT
-            $employee = Employee::find($id);
-            $employee->firstname = $request->firstName;
-            $employee->lastname = $request->lastName;
-            $employee->email = $request->email;
-            $employee->age = $request->age;
-            $employee->position_id = $request->position;
-            $employee->save();
+        }
+        $employee->save();
 
-            return redirect()->route('employees.index');
-     }
-
-
+        return redirect()->route('employees.index');
+    }
     /**
      * Remove the specified resource from storage.
      */
+
     public function destroy(string $id)
     {
-        // // QUERY BUILDER
-        // DB::table('employees')
-        // ->where('id', $id)
-        // ->delete();
-        // return redirect()->route('employees.index');
-        // ELOQUENT
-    Employee::find($id)->delete();
-    return redirect()->route('employees.index');
+        $employee = Employee::find($id);
+        $file ='public/files/'.$employee->encrypted_filename;
 
+
+    if (!empty($file)) {
+        // Hapus file dari direktori public
+        Storage::delete('/' . $file);
+    }
+
+        // Hapus entitas dari database
+        $employee->delete();
+
+    return redirect()->route('employees.index');
+    }
+
+    public function downloadFile($employeeId)
+    {
+    $employee = Employee::find($employeeId);
+    $encryptedFilename = 'public/files/'.$employee->encrypted_filename;
+    $downloadFilename = Str::lower($employee->firstname.'_'.$employee->lastname.'_cv.pdf');
+
+    if(Storage::exists($encryptedFilename)) {
+        return Storage::download($encryptedFilename, $downloadFilename);
+    }
     }
 
 }
